1fc1bf9c3e76423713594e260ee6392d2cc7ae79
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-30 15:47:05 +0530

    feat: add controller

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-require("dotenv").config();
 const express = require("express");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const notFound = require("./middleware/not-found");
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 const app = express();
+require("dotenv").config();
+require("express-async-errors");
 
 //middleware
-app.use(express.json);
+app.use(express.json());
 
 //routes
 app.get("/", (req, res) => {
 	res.send("<h1>Store API</h1><a href='/api/v1/products'>PRoducts</a>");
 });
 
-app.use("/api/v1/products");
+app.use("/api/v1/products", productsRouter);
 
 app.use(errorHandlerMiddleware);
 app.use(notFound);
