7d18500903288e464d75742d96fa7993b713ee05
Shivam77607 <shivamsharma77607@gmail.com>
2023-05-30 11:26:49 +0530

    feat: connect to db

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('04 Store API')
+require("dotenv").config();
+const express = require("express");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFound = require("./middleware/not-found");
+const connectDB = require("./db/connect");
+const app = express();
+
+//middleware
+app.use(express.json);
+
+//routes
+app.get("/", (req, res) => {
+	res.send("<h1>Store API</h1><a href='/api/v1/products'>PRoducts</a>");
+});
+
+app.use(errorHandlerMiddleware);
+app.use(notFound);
+
+const port = process.env.PORT || 3000;
+
+async function start() {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, console.log(`Server is running at port ${port}`));
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+start();
